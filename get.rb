25bfc2e8b9e8b6424503c4de7be97dbe5bb1cf6a
Daniel Bretoi <daniel@otherware.org>
2024-03-26 23:50:45 -0700

    pretty

--- get.rb
+++ get.rb
@@ -1,16 +1,12 @@
 require 'json'
 
-PROFILE="~/Library/Application Support/BraveSoftware/Brave-Browser/*/Bookmarks"
-
-path = File.expand_path(PROFILE)
-
-@items = []
+PROFILE = "~/Library/Application Support/BraveSoftware/Brave-Browser/*/Bookmarks"
+path    = File.expand_path(PROFILE)
+@items  = []
 
 def iterate_bookmarks(branch)
   if branch['type'] == 'folder'
-    branch['children'].each do |child|
-      iterate_bookmarks(child)
-    end
+    branch['children'].each { |child| iterate_bookmarks(child) }
   else
     @items << { title: branch['name'], arg: branch['url'], subtitle: branch['url'] }
   end
@@ -18,9 +14,7 @@ end
 
 Dir.glob(path) do |file_path|
   bookmarks = JSON.parse(IO.read(file_path))
-  bookmarks['roots'].each_value do |branch|
-    iterate_bookmarks(branch)
-  end
+  bookmarks['roots'].each_value { |branch| iterate_bookmarks(branch) }
 end
 
 print JSON.dump({ items: @items  })
