d90c5222610e10b30e5489b85325a09e8a8320a0
Daniel Bretoi <daniel@otherware.org>
2024-03-26 23:40:02 -0700

    fix indent

--- get.rb
+++ get.rb
@@ -6,34 +6,21 @@ path = File.expand_path(PROFILE)
 
 @items = []
 
-# recursivly iterate through the bookmarks where each branch is a root which has children and children is a folder type
 def iterate_bookmarks(branch)
-	if branch['type'] == 'folder'
-		branch['children'].each do |child|
-			iterate_bookmarks(child)
-		end
-	else
-		@items << { title: branch['name'], arg: branch['url'], substring: branch['url'] }
-	end
+  if branch['type'] == 'folder'
+    branch['children'].each do |child|
+      iterate_bookmarks(child)
+    end
+  else
+    @items << { title: branch['name'], arg: branch['url'], subtitle: branch['url'] }
+  end
 end
 
 Dir.glob(path) do |file_path|
-
-	bookmarks = JSON.parse(IO.read(file_path))
-	bookmarks['roots'].each_value do |branch|
-		iterate_bookmarks(branch)
-	end
-	# print json
-  
-  # Example of reading the file, uncomment if needed
-  # File.read(file_path) do |content|
-  #   puts content
-  # end
+  bookmarks = JSON.parse(IO.read(file_path))
+  bookmarks['roots'].each_value do |branch|
+    iterate_bookmarks(branch)
+  end
 end
-
-
-
-query = ARGV[0]
-
 
 print JSON.dump({ items: @items  })
